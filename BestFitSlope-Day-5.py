a4eec4ecddf93cbd58db1cdbbdd9c475cb7e5f18
humayuntanwar <humayuntanwar@gmail.com>
2018-09-03 21:44:10 +0500

    Testing assumptions by creating data set as well

--- BestFitSlope-Day-5.py
+++ BestFitSlope-Day-5.py
@@ -7,10 +7,32 @@ from matplotlib import style
 
 style.use('fivethirtyeight')
 
-xs =np.array([1,2,3,4,5,6], dtype=np.float64)
-ys =np.array([5,4,6,5,6,7], dtype=np.float64)
+#xs =np.array([1,2,3,4,5,6], dtype=np.float64)
+#ys =np.array([5,4,6,5,6,7], dtype=np.float64)
 #plt.scatter(xs,ys) #test show
 #plt.show()
+
+#testing Assumptions that we made
+
+import random # using random numbers
+
+def creat_dataset(hm, variance, step=2, correlation= False): #
+    val = 1 #first val for y
+    ys = []
+    for i in range(hm):
+        y =val+ random.randrange(-variance,variance)
+        ys.append(y)
+        if correlation and correlation =='pos':
+            val+=step
+        elif correlation and correlation =='neg':
+            val-=step
+    xs = [i for i in range(len(ys))]
+
+    return np.array(xs,dtype=np.float64), np.array(ys,dtype=np.float64)
+
+
+
+
 #lets generate best fit slop
 
 def best_fit_slope_and_intercept(xs, ys):
@@ -24,6 +46,8 @@ def best_fit_slope_and_intercept(xs, ys):
 
     return m,b 
 
+xs,ys = creat_dataset(40,80,2, correlation=True)
+
 
 m,b = best_fit_slope_and_intercept(xs,ys)
 print(m,b)
@@ -35,7 +59,7 @@ predict_x = 8
 predict_y = (m*predict_x) +b
 
 plt.scatter(xs,ys)
-plt.scatter(predict_x,predict_y , color='g')
+plt.scatter(predict_x,predict_y ,s=100, color='g')
 plt.plot(xs, regression_line)
 plt.show()
 
