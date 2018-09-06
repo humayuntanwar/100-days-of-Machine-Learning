d78baa6c6f509186b9069dc95cd38ced927a3e82
humayuntanwar <humayuntanwar@gmail.com>
2018-09-06 22:05:15 +0500

    further implentation of support vector machine algorithm

--- SupportVectorMachine-day-9.py
+++ SupportVectorMachine-day-9.py
@@ -13,6 +13,38 @@ class Support_Vector_Machine:
             self.ax = self.fig.add_subplot(1,1,1)
     #train
     def fit(self,data):
+        self.data =data
+        #{||w|| : {w,b}}
+        opt_dict = {}
+
+        transforms = [[1,1],[-1,1],[-1,-1],[1,-1]]
+
+        all_data =[]
+
+        for yi in self.data:
+            for featureset in self.data[yi]:
+                for feature in featureset:
+                    all_data.append(feature)
+        self.max_feature_value = max(all_data)
+        self.min_feature_value = min(all_data)
+        all_data = None
+
+        step_sizes =[self.max_feature_value * 0.1,self.max_feature_value * 0.01,
+                     # point of expense:
+                     self.max_feature_value * 0.001]
+        
+        #extremely expensive
+        b_range_multiple = 5
+        #
+        b_multiple = 5
+        latest_optimum = self.max_feature_value*10
+
+        for step in step_sizes:
+            w = np.array([latest_optimum,latest_optimum])
+            #we can do this because convex
+            optimized = False
+            while not optimized:
+                #pass
         pass
 
     def predict(self,data):
