d398a9adb6854129debef96c7e654202a7e35bc0
humayuntanwar <humayuntanwar@gmail.com>
2018-09-07 20:01:24 +0500

    support vector algorithm tested

--- SupportVectorMachine-day-9-10.py
+++ SupportVectorMachine-day-9-10.py
@@ -86,10 +86,58 @@ class Support_Vector_Machine:
     def predict(self,data):
         # sign (x.w+b)
         classification = np.sign(np.dot(np.array(features),self.w)+self.b)
+        if classification !=0 and self.visualization:
+            self.ax.scatter(features[0],features[1], s=200, marker=*,c=self.colors[classification])
+        
         return classification
+    # only for vislization
+    def visualize(self):
+        [[self.ax.scatter(x[0],x[1],s=100,color=self.colors[i]) for x in data_dict[i]] for i in data.dict]
+
+        #hyperplane x, w+b
+        # v = x.w +b
+        #ps =1 
+        #neg = 1
+        #dec = 0
+        #plot data points, show support vector and decision boundary hyper plane
+        def hyperplane(x,w,b,v):
+            return (-w[0] * x-b +v / w[1])
+
+        datarange = (self.min_feature_value * 0.9, self.max_feature_value * 1.1)
+        hyp_x_min = datarange[0]
+        hyp_x_max = datarange[1]
+
+        # (w.x+b) = 1
+        #positive suport vector hyperplane(will be a scalar value), will be y
+        psv1 = hyperplane(hype_x_min, self.w,self.b,1)
+        psv2 = hyperplane(hype_x_max, self.w,self.b,1)
+        self.ax.plot([hyp_x_min,hyp_x_max],[psv1,psv2])
+
+        # (w.x+b) = -1
+        #negative suport vector hyperplane(will be a scalar value), will be y
+        nsv1 = hyperplane(hype_x_min, self.w,self.b,-1)
+        nsv2 = hyperplane(hype_x_max, self.w,self.b,-1)
+        self.ax.plot([hyp_x_min,hyp_x_max],[nsv1,nsv2])
+
+         # (w.x+b) = 0
+        #positive suport vector hyperplane(will be a scalar value), will be y
+        db1 = hyperplane(hype_x_min, self.w,self.b,0)
+        db2 = hyperplane(hype_x_max, self.w,self.b,0)
+        self.ax.plot([hyp_x_min,hyp_x_max],[db1,db2])
+
+
+        plt.show()
+
+
+
 
 
 
 #create sample data dictionary
 data_dict = {-1:np.array([[1,7],[2,8],[3,8],]),
             1:np.array([[5,1],[6,-1],[7,3],])}
+
+
+svm = Support_Vector_Machine()
+svm.fit(data=data_dict)
+svm.visualize()
