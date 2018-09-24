d5dc863100e7d5c9c9d184e36adc6cc50eaeb614
humayuntanwar <humayuntanwar@gmail.com>
2018-09-24 16:18:26 +0500

    traing of model completed

--- OpenAI-Gym/OpenAi-training-cartpole-day26.py
+++ OpenAI-Gym/OpenAi-training-cartpole-day26.py
@@ -94,8 +94,52 @@ def initial_population():
 
     return training_data
 
-
+#### DAY 27
 
 #some_random_games_first()
 
-initial_population()
+#initial_population()
+#create our nueral network model
+# input_size ,sperate model from triaing and testing
+def nueral_network_model(input_size):
+    #input layer
+    network = input_data(shape=[None,input_size,1],name='input')
+    #full connected layer
+    network = fully_connected(network,128,activation='relu')
+    network = dropout(network,0.8)
+    
+    network = fully_connected(network,256,activation='relu')
+    network = dropout(network,0.8)
+
+    network = fully_connected(network,512,activation='relu')
+    network = dropout(network,0.8)
+
+    network = fully_connected(network,256,activation='relu')
+    network = dropout(network,0.8)
+
+    network = fully_connected(network,128,activation='relu')
+    network = dropout(network,0.8)
+
+    #output layer
+    network = fully_connected(network,2,activation='softmax')
+    network = regression(network,optimizer='adam',learning_rate=LR,loss='categorical_crossentropy',name='targets')
+
+    model = tflearn.DNN(network,tensorboard_dir='log')
+
+    return model
+
+#train model
+def train_model(training_data, model=False):
+    #observations
+    X = np.array([i[0] for i in training_data]).reshape(-1,len(training_data[0]),1)
+    y = [i[1] for i in training_data]    
+    
+    if not model:
+        model = nueral_network_model(input_size =len(X[0]))
+    
+    model.fit({'input':X},{'targets':y},n_epoch=3,snapshot_step=500,show_metric=True,run_id='openaistuff')
+
+    return model
+
+training_data = initial_population()
+model = train_model(training_data)
